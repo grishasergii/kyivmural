cf9a67c09e3594310b6e01006023e90d58de1e51
Sergii Gryshkevych <s.gryshkevych@gmail.com>
2016-12-19 21:04:49 +0100

    Fixed remote_addr

--- app/__init__.py
+++ app/__init__.py
@@ -21,7 +21,7 @@ class ContextualFilter(logging.Filter):
     def filter(self, log_record):
         log_record.utcnow = (datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S,%f %Z'))
         log_record.url = request.path
-        log_record.ip = request.remote_addr
+        log_record.ip = request.environ['REMOTE_ADDR']
         return True
 
 bootstrap = Bootstrap()
